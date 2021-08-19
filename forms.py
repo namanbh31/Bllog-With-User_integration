20dd616ea41cd06801c99738c5184ef586f6e146
namanbhardwaj <namanbh3193@gmail.com>
2021-08-19 11:12:47 +0530

    removed useless module

--- forms.py
+++ forms.py
@@ -2,7 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
-from email_validator import ValidatedEmail
 
 ##WTForm
 class CreatePostForm(FlaskForm):
